8ec9f669f16858f9397ebf9f07982cb40847ae3e
Nicholas Macrae Hodge <nhodge1@hydra23.eecs.utk.edu>
2020-03-12 16:44:16 -0400

    first commit

---
+++ combat.cpp
@@ -0,0 +1,27 @@
+
+
+using namespace std;
+
+struct Enemy{
+
+	int hp;
+	vector <int> attacks;
+
+
+}
+
+struct Player{
+
+	int hp;
+	int will;
+
+
+}
+
+struct Attacks{
+
+	string name;
+	int damage;
+
+
+}
