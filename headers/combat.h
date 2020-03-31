1d1e48f410f7630582dbbd2934d2489e7301ee47
Jack Hutchins <jhutch26@hydra24.eecs.utk.edu>
2020-03-31 18:38:12 -0400

    Added functions to inventory, merged header files

--- headers/combat.h
+++ headers/combat.h
@@ -1,31 +1,65 @@
+#ifndef COMBAT_H
+#define COMBAT_H
 #include <vector>
 #include <string>
+#include <map>
 
 using namespace std;
 
-struct Item{
+struct Player;
 
+struct PassiveItem {
+    string name;
+    string description;
+    int willInc = 0;        //amount an item increases willpower, -1 means return to max
+    int maxWillInc = 0;
+	int strengthInc = 0;    //amount an item increases strength by
+    int defenceInc = 0;     //amount an item increases defence by
+    int agilityInc = 0;     //amount an item increases agility by
+    int accuracyInc = 0;    //amount an item increases accuracy by
 };
 
-struct Weapon{
-
+//formulas for how player stats interact with weapon stats, the accuracy one is a little weird, might need some work
+//AttackAccuracy = ln(PlayerAcc*WeaponAcc)*(25/ln(10))
+//AttackSpeed = ((agility/100)+1)*WeaponSpeed
+//AttackDamage = ((strength/100)+1)*WeaponDamage
+struct ActiveItem {
+    string name;
+    string description;
+    int damage;     //how much damage a successfull hit does, if a crit is rolled damage is doubled
+    int accuracy;   //0-100, a number is rolled between 0 and 100, any number lower than
+                    //accuracy is a hit, critChance is then rolled
+    int speed;      //the speed determines turn order, agility is used as a multiplier for this
+    int critChance; //0-100, a number between 0 and 100 is rolled and results 
+                    //in a crit for any number less than this number
 };
 
-struct Attack{
-
-	string name;
-	int damage;
-
+class Items {
+public:
+    Items();
+    bool applyItem(string itemName, Player &player);
+    bool addWeapon(string weaponName, Player &player);
+    PassiveItem getItem(string itemName);
+    ActiveItem getWeapon(string weaponName);
+	int getAttAccuracy(string weaponName, Player &player);
+	int getAttSpeed(string weaponName, Player &player);
+	int getAttDmg(string weaponName, Player &player);
+private:
+    map<string, PassiveItem> pItems;
+    map<string, ActiveItem> aItems;
 };
 
 struct Player{
 
-	int hp;
+	int maxWillpower;
 	int willpower;
+	int strength;
+	int defence;
+	int agility;
+	int accuracy;
 
-	vector <Item> items;
-	vector <Weapon> weapons;
-	vector <Attack> attacks;
+	vector <PassiveItem> items;
+	vector <ActiveItem> weapons;
 
 	void TakeDamage(int amount);
 
@@ -39,3 +73,5 @@ struct Enemy{
 	void TakeDamage(int amount);
 
 };
+
+#endif
--- src/inventory.cpp
+++ src/inventory.cpp
@@ -1,55 +1,107 @@
 #include <fstream>
 #include <iostream>
-#include <vector>
-#include <inventory.h>
+#include <map>
+#include <cmath>
+//#include "../headers/inventory.h"
+#include "../headers/combat.h"
 using namespace std;
 
-bool itemInit(vector <Item> &v) {
+Items::Items() {
     fstream fin;
     int numModifiers, statInc;
     string stat;
-    string fileName = "data/items.txt";
-    fin.open(("data/items.txt"));
+    fin.open(("../data/PassiveItems.txt"));
 
-    if (fin.fail) return false;
+    //if (fin.fail()) return false;
 
     while (fin >> numModifiers) {
-        Item item;
+        PassiveItem item;
+        getline(fin, item.name);
+        getline(fin, item.description);
         for (int i = 0; i < numModifiers; i++) {
             fin >> stat;
 
-            switch (stat) {
-            case "name": 
-                fin >> item.name;
-                break;
-
-            case "description":
-                fin >> item.description;
-                break;
-
-            case "healthInc":
-                fin >> item.healthInc;
-                break;
-
-            case "willInc":
-                fin >> item.willInc;
-                break;
-                
-            case "damageInc":
-                fin >> item.damageInc;
-                break;
-
-            case "defenceInc":
-                fin >> item.defenceInc;
-                break;
-            
-            default:
-                return false;
-                break;
-            }
-            
-            v.push_back(item);
+            if(stat == "will") fin >> item.willInc;
+            else if(stat == "maxWill") fin >> item.maxWillInc;
+            else if(stat == "str") fin >> item.strengthInc;
+            else if(stat == "def") fin >> item.defenceInc;
+            else if(stat == "agl") fin >> item.agilityInc;
+            else if(stat == "acc") fin >> item.accuracyInc;
         }
+        pItems.insert(pair<string, PassiveItem>(item.name, item));
     }
+
+    fin.close();
+    fin.open("../data/ActiveItems.txt");
+    //if (fin.fail()) return false;
+
+    while (fin >> numModifiers) {
+        ActiveItem item;
+        getline(fin, item.name);
+        getline(fin, item.description);
+        for (int i = 0; i < numModifiers; i++) {
+            fin >> stat;
+
+            if(stat == "dmg") fin >> item.damage;
+            else if(stat == "acc") fin >> item.accuracy;
+            else if(stat == "spd") fin >> item.speed;
+            else if(stat == "crit") fin >> item.critChance;
+        }
+        aItems.insert(pair<string, ActiveItem>(item.name, item));
+    }
+}
+
+bool Items::applyItem(string itemName, Player &player) {
+    if (pItems.find(itemName) == pItems.end()) return false;
+    PassiveItem item = pItems.find(itemName)->second;
+
+    player.accuracy += item.accuracyInc;
+    player.agility += item.agilityInc;
+    player.defence += item.defenceInc;
+    player.strength += item.strengthInc;
+    player.maxWillpower += item.maxWillInc;
+    
+    if (item.willInc == -1) player.willpower = player.maxWillpower;
+    else player.willpower += item.willInc;
+
+    if (player.willpower > player.maxWillpower) player.willpower = player.maxWillpower; 
+
+    player.items.push_back(item);
+
     return true;
+}
+
+bool Items::addWeapon(string weaponName, Player &player) {
+    if (aItems.find(weaponName) == aItems.end()) return false;
+    ActiveItem weapon = aItems.find(weaponName)->second;
+
+    player.weapons.push_back(weapon);
+
+    return true;
+}
+
+PassiveItem Items::getItem(string itemName) {
+    return pItems.find(itemName)->second;
+}
+
+ActiveItem Items::getWeapon(string weaponName) {
+    return aItems.find(weaponName)->second;
+}
+
+//AttackAccuracy = ln(PlayerAcc*WeaponAcc)*(25/ln(10))
+int Items::getAttAccuracy(string weaponName, Player &player) {
+    if (aItems.find(weaponName) == aItems.end()) return -1;
+    return log(player.accuracy * aItems.find(weaponName)->second.accuracy) * (25./log(10));
+}
+
+//AttackSpeed = ((agility/100)+1)*WeaponSpeed
+int Items::getAttSpeed(string weaponName, Player &player) {
+    if (aItems.find(weaponName) == aItems.end()) return -1;
+    return ((((double) player.agility)/100)+1) * (double) aItems.find(weaponName)->second.speed;
+}
+
+//AttackDamage = ((strength/100)+1)*WeaponDamage
+int Items::getAttDmg(string weaponName, Player &player) {
+    if (aItems.find(weaponName) == aItems.end()) return -1;
+    return ((((double) player.strength)/100)+1) * (double) aItems.find(weaponName)->second.damage;
 }
