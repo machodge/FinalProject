f7781c75aa04537c6b9a4a3ab1b4375e1f547655
Nicholas Macrae Hodge <nhodge1@hydra23.eecs.utk.edu>
2020-04-21 17:16:37 -0400

    file management

--- include/game.h
+++ include/game.h
@@ -1,1 +1,39 @@
-//
+#include<algorithm>
+#include<iostream>
+#include<map>
+using namespace std;
+
+struct Player
+{
+    int Courage;
+    int Willpower;
+    int Strength;
+    int Agility;
+    int Accuracy;
+    int Defense ;
+    bool BoneSaw;
+    bool Scythe;
+    bool DOG;
+    bool Scalpel;
+};
+/*Action data structure to hold all data pertaining to
+  actions*/
+struct Action
+{
+    bool check;
+    string item;
+    string description;
+    pair <int, int> lock;
+};
+/*Room data structure to hold all data pertaining to
+rooms*/
+struct Room
+{
+    bool lock;                      //Wheter or not room is locked
+    string description;             //Room description
+    pair <int, int> loc;            //Room location
+    map<string, Action *>  actions; //Allowed actions in room
+    vector<string> directions;      //Allowed directions
+    map<string, Room *> adj;        //Accessable points from room
+};
+
